e58d5fef98dcda8d7b3100976e59562b7a9b00f9
Administrator <b2bawany@gmail.com>
2025-10-08 11:15:47 +0500

    tasks functions within object with method and properties

--- muzzammil.com/frontend/js/object.js
+++ muzzammil.com/frontend/js/object.js
@@ -47,3 +47,48 @@ function newPerson(fname, lname, age, color, language){
 
 let person1 = new newPerson("Ali", "Khan", 22, "white", "Urdu");
 let person2 = new newPerson("Ahmed", "Raza", 25, "black", "English");
+
+//8
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: function() { //this is called method
+        return this.width * this.height;
+    }
+};
+
+console.log(rectangle.area());
+
+//9
+const account = {
+    balance: 1000,
+    deposit: function(amount) {
+        this.balance += amount;
+        console.log(`Deposited: $${amount}. New balance: $${this.balance}`);
+    },
+    withdraw: function(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`Withdrew: $${amount}. New balance: $${this.balance}`);
+        } else {
+            console.log("Insufficient funds");
+        }
+    }   
+};
+
+account.deposit(500);
+account.withdraw(300);
+account.withdraw(1500);
+
+
+//10
+const movie = {
+    title: "Inception",
+    rating: [4,6,5,5,3,4,7,5,6,3],
+    averageRating: function() {
+        const sum = this.rating.reduce((total, rating) =>total + rating, 0);
+        return (sum / this.rating.length).toFixed(2);
+    }
+};
+
+console.log(`Average Rating ${movie.title}: ${movie.averageRating()}`);
